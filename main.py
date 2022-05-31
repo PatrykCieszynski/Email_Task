405bf42c6ca11a72143398dba8d0729b3e140b9f
Patryk Cieszyński <patrykciesz@gmail.com>
2022-05-31 13:20:02 +0200

    First task(classes, opening files and reading them, validating emails)

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from os import listdir
+import re
+import csv
 
 
-# Press the green button in the gutter to run the script.
+class Email:
+    container = []
+    bad_emails = []
+
+    def __init__(self, local_part, domain, last_part):
+        self.local_part = local_part
+        self.domain = domain
+        self.last_part = last_part
+
+    @staticmethod
+    def match_email(email):
+        PATTERN = re.compile("(.+)@(.+)\.([a-zA-Z0-9]{1,4})")
+        if PATTERN.match(email):
+            Email.container.append(
+                Email(PATTERN.match(email).group(1), PATTERN.match(email).group(2), PATTERN.match(email).group(3)))
+        else:
+            Email.bad_emails.append(email.split("\n")[0])
+
+
+class File:
+    PATH = "./Resources/emails/"
+    container = []
+
+    def __init__(self):
+        for filename in listdir(self.PATH):
+            file = open(self.PATH + filename, 'r')
+            self.parse_file(file, filename.split('.')[1])
+            file.close()
+
+    @staticmethod
+    def parse_file(file, extension):
+        if extension == "txt":
+            lines = file.readlines()
+            for line in lines:
+                Email.match_email(line)
+        else:
+            csvfile = csv.DictReader(file, delimiter=";")
+            for row in csvfile:
+                Email.match_email(row["email"])
+
+
+def show_incorrect_emails():
+    print("Invalid emails (" + str(len(Email.bad_emails)) + "):")
+    for email in Email.bad_emails:
+        print("\t" + email)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    emails = Email
+    files = File()
+    show_incorrect_emails()
