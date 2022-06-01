4c314928d5d9ef1eff27c89250ddeb140cfe8cd3
Patryk Cieszyński <patrykciesz@gmail.com>
2022-06-01 10:48:50 +0200

    Third Task done (Grouping emails by domain) Merged Email "domain" and "last_part" attributes to "domain" Changed Email.getMail to __str__ Changed Emails container to set to avoid duplicates Removed static container from File class, cuz its was useless Changed PATTERN and PATH "constants" to global Program now only reads txt and csv files, skips other extension

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 from os import listdir
 import re
 import csv
+from itertools import groupby
+
+PATTERN = re.compile('(.+)@(.+)\.([a-zA-Z0-9]{1,4})')
+PATH = "./Resources/emails/"
 
 
 class Email:
-    container = []
+    container = set()
     bad_emails = []
 
-    def __init__(self, local_part, domain, last_part):
+    def __init__(self, local_part, domain):
         self.local_part = local_part
         self.domain = domain
-        self.last_part = last_part
 
     @staticmethod
     def match_email(email):
-        PATTERN = re.compile("(.+)@(.+)\.([a-zA-Z0-9]{1,4})")
         if PATTERN.match(email):
-            Email.container.append(
-                Email(PATTERN.match(email).group(1), PATTERN.match(email).group(2), PATTERN.match(email).group(3)))
+            Email.container.add(
+                Email(PATTERN.match(email).group(1),
+                      PATTERN.match(email).group(2) + "." + PATTERN.match(email).group(3)))
         else:
             Email.bad_emails.append(email.split("\n")[0])
 
+    def __str__(self):
+        return self.local_part + "@" + self.domain
 
-    def getEmail(self):
-        return self.local_part + "@" + self.domain + "." + self.last_part
+    def __eq__(self, other):
+        return self.local_part == other.local_part and self.domain == other.domain
+
+    def __hash__(self):
+        return hash(("local_part", self.local_part,
+                    "domain", self.domain))
 
 
 class File:
-    PATH = "./Resources/emails/"
-    container = []
-
     def __init__(self):
-        for filename in listdir(self.PATH):
-            file = open(self.PATH + filename, 'r')
+        for filename in listdir(PATH):
+            file = open(PATH + filename, 'r')
             self.parse_file(file, filename.split('.')[1])
             file.close()
 
@@ -42,7 +48,7 @@ class File:
             lines = file.readlines()
             for line in lines:
                 Email.match_email(line)
-        else:
+        elif extension == "csv":
             csvfile = csv.DictReader(file, delimiter=";")
             for row in csvfile:
                 Email.match_email(row["email"])
@@ -60,7 +66,7 @@ def search_emails_by_text(string):
     for email in Email.container:
         if string in email.local_part:
             counter += 1
-            found_emails.append(email.getEmail())
+            found_emails.append(email)
     if counter > 0:
         print("Found emails with '" + string + "' in email (" + str(counter) + "):")
         for email in found_emails:
@@ -69,8 +75,18 @@ def search_emails_by_text(string):
         print("Email not found")
 
 
+def group_by_domain():
+    sorted_emails = sorted(Email.container, key=lambda x: (x.domain, x.local_part))
+    grouped_emails = [list(domains) for key, domains in groupby(sorted_emails, key=lambda x: x.domain)]
+    for domain in grouped_emails:
+        print("Domain " + domain[0].domain + " (" + str(domain.__len__()) + "):")
+        for email in domain:
+            print("\t" + str(email))
+
+
 if __name__ == '__main__':
     emails = Email
     files = File()
-    show_incorrect_emails()
-    search_emails_by_text("agustin")
+    # show_incorrect_emails()
+    # search_emails_by_text("agustin")
+    # group_by_domain()
