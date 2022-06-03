0ea7c0fb7391b447ff666549ff2bab501bad5bcc
Patryk Cieszyński <patrykciesz@gmail.com>
2022-06-03 18:17:48 +0200

    Added tests Fixed some bugs / unclosed files

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ PATH = "./Resources/emails/"
 
 
 class EmailContainer:
-
-    def __init__(self, path):
+    def __init__(self, path=None):
         self.container = set()
         self.bad_emails = []
         self.emails_sent = set()
-        self.parse_files(path)
+        if path is not None:
+            self.parse_files(path)
 
     def parse_files(self, path):
-        for filename in listdir(path):
-            extension = filename.split('.')[1]
+        files = []
+        try:
+            files = listdir(path)
+        except FileNotFoundError:
+            print("Emails directory not found")
+            quit()
+        for filename in files:
+            extension = (filename.split('.')[1]).lower()
             file = open(path + filename, 'r')
             if extension == "txt":
                 lines = file.readlines()
@@ -53,7 +59,7 @@ class EmailContainer:
         if counter > 0:
             print("Found emails with '" + string + "' in email (" + str(counter) + "):")
             for email in found_emails:
-                print(email)
+                print("\t" + str(email))
         else:
             print("Email not found")
 
@@ -66,15 +72,16 @@ class EmailContainer:
                 print("\t" + str(email))
 
     def parse_log_file(self, path_to_log_file):
+        file = None
         try:
-            extension = path_to_log_file.split('.')[-1]
+            extension = (path_to_log_file.split('.')[-1]).lower()
             if extension == "logs":
                 file = open(path_to_log_file, 'r')
                 lines = file.readlines()
                 for line in lines:
                     self.emails_sent.add(Email.match_email(re.search("\'(.+)\'", line).group(1)))
             else:
-                raise ValueError
+                raise ValueError()
         except AttributeError:
             print("Log file has wrong format")
             quit()
@@ -87,6 +94,9 @@ class EmailContainer:
         except ValueError:
             print("Log file has wrong extension")
             quit()
+        finally:
+            if file is not None:
+                file.close()
 
     def find_emails_not_in_logs(self, path_to_log_file):
         self.parse_log_file(path_to_log_file)
@@ -98,18 +108,18 @@ class EmailContainer:
 
 
 class Email:
-
     def __init__(self, local_part, domain):
         self.local_part = local_part
         self.domain = domain
 
     @staticmethod
     def match_email(email):
-        if PATTERN.match(email):
-            return Email(PATTERN.match(email).group(1),
-                         PATTERN.match(email).group(2) + "." + PATTERN.match(email).group(3))
-        else:
-            return email.split("\n")[0]
+        if email is not None:
+            if PATTERN.match(email):
+                return Email(PATTERN.match(email).group(1),
+                             PATTERN.match(email).group(2) + "." + PATTERN.match(email).group(3))
+            else:
+                return email.split("\n")[0]
 
     def __str__(self):
         return self.local_part + "@" + self.domain
